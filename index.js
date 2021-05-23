efd4ab5175abfe724d4a707648c1bab43105ce27
Giancarlo Guerra <gian.guerra23@gmail.com>
2021-05-22 22:45:13 -0500

    completing 'process'

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ let notes = [
 
 /*INICIO Y ESCUCHA DEL SERVIDOR---------------------------------------------------------------- */
 
-    const PORT= proces.env.PORT || 3001;
+    const PORT= process.env.PORT || 3001;
     
     app.listen(PORT,()=>{
           console.log(`Server running on port ${PORT}`);
